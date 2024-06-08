53b808280f71ca097fbb69489d1aeee8229c8906
02vandup11 <138954560+02vandup11@users.noreply.github.com>
2024-06-08 05:32:30 +0530

    Update connect.php added azure connect code

--- config/connect.php
+++ config/connect.php
@@ -1,12 +1,12 @@
-<?php
-$hostname="localhost";
-$username="root";
-$password="";
-$database="onlineweb";
-$con=mysqli_connect($hostname,$username,$password,$database);
-if(mysqli_connect_errno()){
-    echo("not connected");
-}
+
+// $hostname="localhost";
+// $username="root";
+// $password="";
+// $database="onlineweb";
+// $con=mysqli_connect($hostname,$username,$password,$database);
+// if(mysqli_connect_errno()){
+//     echo("not connected");
+
 
 // $hostname="localhost";
 // $username="id21743933_vanduu";
@@ -16,4 +16,16 @@ if(mysqli_connect_errno()){
 // if(mysqli_connect_errno()){
 //     echo("not connected");
 // }
+
+<?php
+$serverName = getenv('DB_SERVER');
+$username = getenv('DB_USERNAME');
+$password = getenv('DB_PASSWORD');
+$database = getenv('DB_DATABASE');
+
+$conn = mysqli_connect($serverName, $username, $password, $database);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 ?>
