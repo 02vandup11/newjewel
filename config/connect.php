65c641adc0984020e17d2fd1ce2fea5d801d86ae
02vandup11 <138954560+02vandup11@users.noreply.github.com>
2024-06-09 16:58:31 +0530

    Update connect.php

--- config/connect.php
+++ config/connect.php
@@ -17,8 +17,7 @@
 //     echo("not connected");
 // }
 
-<?php
-$serverName = getenv('DB_SERVER');
+<!-- $serverName = getenv('DB_SERVER');
 $username = getenv('DB_USERNAME');
 $password = getenv('DB_PASSWORD');
 $database = getenv('DB_DATABASE');
@@ -27,5 +26,21 @@ $conn = mysqli_connect($serverName, $username, $password, $database);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
+} -->
+
+<?php
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:vanserver.database.windows.net,1433; Database = vandatabase", "azureuser", "Van12345");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "azureuser", "pwd" => "Van12345", "Database" => "vandatabase", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:vanserver.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
