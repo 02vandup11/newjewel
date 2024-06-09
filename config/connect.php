879fc0831523c3518e503e88f7c32c1cde68c064
02vandup11 <138954560+02vandup11@users.noreply.github.com>
2024-06-09 17:16:08 +0530

    Update connect.php

--- config/connect.php
+++ config/connect.php
@@ -1,32 +1,4 @@
 
-// $hostname="localhost";
-// $username="root";
-// $password="";
-// $database="onlineweb";
-// $con=mysqli_connect($hostname,$username,$password,$database);
-// if(mysqli_connect_errno()){
-//     echo("not connected");
-
-
-// $hostname="localhost";
-// $username="id21743933_vanduu";
-// $password="Vandana@02";
-// $database="id21743933_jewellery";
-// $con=mysqli_connect($hostname,$username,$password,$database);
-// if(mysqli_connect_errno()){
-//     echo("not connected");
-// }
-
-<!-- $serverName = getenv('DB_SERVER');
-$username = getenv('DB_USERNAME');
-$password = getenv('DB_PASSWORD');
-$database = getenv('DB_DATABASE');
-
-$conn = mysqli_connect($serverName, $username, $password, $database);
-
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-} -->
 
 <?php
 // PHP Data Objects(PDO) Sample Code:
