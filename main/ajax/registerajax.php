d82f3d53e6f8c2d25f664e6cce903d4fe36f2a88
02vandup11 <138954560+02vandup11@users.noreply.github.com>
2024-06-11 08:44:28 +0000

    change to ajax

--- main/ajax/registerajax.php
+++ main/ajax/registerajax.php
@@ -1,21 +1,31 @@
 <?php
 include("../../config/connect.php");
-if(
-    !empty($_POST['firstName'])&& !empty($_POST['lastName'])&& !empty($_POST['email'])&& !empty($_POST['phoneNumber'])&& !empty($_POST['password'])
-)
-$firstName=$_POST['firstName'];
-$lastName=$_POST['lastName'];
-$email=$_POST['email'];
-$phoneNumber=$_POST['phoneNumber'];
-$password=$_POST['password'];
 
-$registerquerry="INSERT INTO `customer` (`cus_email`, `cus_password`, `cus_fname`, `cus_lname`, `cus_phone`) VALUES ('$email', '$password', '$firstName', '$lastName', '$phoneNumber')";
-$executequerry=mysqli_query($con,$registerquerry);
-if($executequerry)
-{
-    echo("1");
-}
-else{
-    echo("2");
+if (
+    !empty($_POST['firstName']) &&
+    !empty($_POST['lastName']) &&
+    !empty($_POST['email']) &&
+    !empty($_POST['phoneNumber']) &&
+    !empty($_POST['password'])
+) {
+    $firstName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
+    $email = $_POST['email'];
+    $phoneNumber = $_POST['phoneNumber'];
+    $password = $_POST['password'];
+
+    // Using prepared statement to prevent SQL injection
+    $registerQuery = "INSERT INTO `customer` (`cus_email`, `cus_password`, `cus_fname`, `cus_lname`, `cus_phone`) VALUES (?, ?, ?, ?, ?)";
+    $statement = mysqli_prepare($con, $registerQuery);
+    mysqli_stmt_bind_param($statement, "sssss", $email, $password, $firstName, $lastName, $phoneNumber);
+    $executeQuery = mysqli_stmt_execute($statement);
+
+    if ($executeQuery) {
+        echo "1"; // Success
+    } else {
+        echo "2"; // Failure
+    }
+} else {
+    echo "0"; // Invalid input
 }
 ?>
