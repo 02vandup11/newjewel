1093627ee4520b1ac294b6594e031e2843959997
02vandup11 <138954560+02vandup11@users.noreply.github.com>
2024-06-11 11:05:16 +0530

    Update connect.php

--- config/connect.php
+++ config/connect.php
@@ -3,7 +3,7 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:akshserver.database.windows.net,1433; Database = akshdatabase", "azureuser", "Aksh@12345");
+    $conn = new PDO("sqlsrv:server = tcp:jewelserver.database.windows.net,1433; Database = Jeweldb", "Azureuser", "Akshada@12345");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -12,7 +12,7 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "azureuser", "pwd" => "Aksh@12345", "Database" => "akshdatabase", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:akshserver.database.windows.net,1433";
+$connectionInfo = array("UID" => "Azureuser", "pwd" => "Akshada@12345", "Database" => "Jeweldb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:jewelserver.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
