e627dd2e6d3d1eba438e45f74550e244ca1843bc
02vandup11 <138954560+02vandup11@users.noreply.github.com>
2024-06-11 08:38:24 +0000

    cahnge to connect.php

--- config/connect.php
+++ config/connect.php
@@ -1,16 +1,9 @@
 <?php
-// PHP Data Objects(PDO) Sample Code:
 try {
     $conn = new PDO("sqlsrv:server = tcp:vanserver.database.windows.net,1433; Database = vandb", "azureuser", "Akshada@12345");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
+} catch (PDOException $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
 }
-
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "azureuser", "pwd" => "Akshada@12345", "Database" => "vandb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:vanserver.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
